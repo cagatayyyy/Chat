771eae9b60e737cbb13da2336bce8cf58a3ddeb3
cagatayyyy <aksoyazilim@outlook.com>
2019-11-24 19:02:35 +0300

    Proje-2

---
+++ server.js
@@ -0,0 +1,28 @@
+var socket = require('socket.io'),
+    http = require('http'),
+    server = http.createServer(),
+    socket = socket.listen(server);
+
+
+server.listen("3000", function () {
+    console.log('Server started');
+});
+
+
+
+
+socket.on('connection', function (connection) {
+
+    connection.on('onlinejoin', function () {
+        socket.emit('feed', 'OLDUUUUUUUUU');
+    });
+
+
+    
+
+
+});
+
+
+
+
